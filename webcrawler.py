c568c5bc3b32a6463d8d11fb326b88d60f23b605
WinterCricket <hodhodhod2003@gmail.com>
2017-02-03 08:30:11 -0600

    Add add procedure to crawl file

--- webcrawler.py
+++ webcrawler.py
@@ -1,5 +1,5 @@
 page =('<div id="top_bin"><div id="top_content" class="width960">'
-'<div class="udacity float-left"><a href="http://udacity.com">')
+'<div class="udacity float-left"><a href="http://udacity.com">polymorphic desire so much of a <a href="http://www.wikipedia.org"> flying sideways like a goose during a hurricane.')
 
 
 #start_link = page.find('<a href="http://'[9:])
@@ -7,8 +7,10 @@ page =('<div id="top_bin"><div id="top_content" class="width960">'
 #url = page[start_link: end_link]
 #print url
 
-start_link = page.find('<a href=')
-start_quote = page.find('"', start_link)
-end_quote = page.find('"', start_quote + 1)
-url = page[start_quote + 1: end_quote]
-print url
+
+def to_get_target(s):
+    start_link = s.find('<a href=')
+    start_quote = s.find('"', start_link)
+    end_quote = s.find('"', start_quote + 1)
+    url = s[start_quote + 1 : end_quote] 
+    return url, s[end_quote:] 
